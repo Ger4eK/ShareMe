0e704d9bb619e488c1f1c05a63b783576e871a60
Ger4eK <ger4ek228@gmail.com>
2022-10-29 22:26:35 +0300

    google login

--- frontend/src/features/Login.tsx
+++ frontend/src/features/Login.tsx
@@ -1,7 +1,74 @@
-import shareVideo from '../assets/share.mp4';
+import shareVideo from '../shared/assets/share.mp4';
+import logo from '../shared/assets/logowhite.png';
+import { FcGoogle } from 'react-icons/fc';
+import GoogleLogin from 'react-google-login';
+import { useEffect } from 'react';
+import { loadGapiInsideDOM } from 'gapi-script';
+import { useNavigate } from 'react-router-dom';
+import { client } from '../shared/client';
 
 const Login = () => {
-  return <div>Login </div>;
+  const navigate = useNavigate();
+  const responseGoogle = (response: any) => {
+    localStorage.setItem('user', JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
+    const doc = {
+      _id: googleId,
+      _type: 'user',
+      userName: name,
+      image: imageUrl,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate('/', { replace: true });
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      await loadGapiInsideDOM();
+    })();
+  });
+
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className=' relative w-full h-full'>
+        <video
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className='w-full h-full object-cover'
+        >
+          <source src={shareVideo} type='video/mp4' />
+        </video>
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay'>
+          <div className='p-5'>
+            <img src={logo} width='130px' />
+          </div>
+
+          <div className='shadow-2xl'>
+            <GoogleLogin
+              clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+              scope='email'
+              render={(renderProps) => (
+                <button
+                  type='button'
+                  className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className='mr-4' /> Sign in with google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/shared/@types/assets.d.ts
+++ frontend/src/shared/@types/assets.d.ts
@@ -2,3 +2,7 @@ declare module "*.mp4" {
    const content: any;
    export default content;
  }
+declare module "*.png" {
+   const content: any;
+   export default content;
+ }
